51ff1655e9b153f7756dde94cb2eaafb077faefb
권현우 <hyeonu.gwon@tisquare.com>
2023-06-20 17:59:09 +0900

    Add | cloud type server response 리다이렉션 http 문제로 meta tag 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
-      <Head />
+      <Head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body>
         <div id="portal" />
         <Main />
